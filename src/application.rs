9983c2c533110ff5d4137e639c94f654142d9932
aclueless <61309385+aclueless@users.noreply.github.com>
2020-06-07 17:03:12 +0700

    small clean up

--- src/application.rs
+++ src/application.rs
@@ -1,32 +1,10 @@
 use wasm_bindgen::UnwrapThrowExt;
 
 pub trait Application: crate::component::Component {
-    // fn mount_to_element(self, root: web_sys::Element) {
-    //     let rc_comp = crate::component::RcComp::with_state_and_element(self, Some(root));
-    //     rc_comp.first_render();
-
-    //     // It is the main component of the app, hence it is reasonable to just forget it.
-    //     std::mem::forget(rc_comp);
-    // }
-
-    // fn mount_to(self, id: &str) {
-    //     let root = crate::utils::document()
-    //         .get_element_by_id(id)
-    //         .expect_throw("No element associated with the specified id (to use as a root element)");
-    //     self.mount_to_element(root);
-    // }
-
-    // fn mount_to_body(self) {
-    //     let root = crate::utils::document()
-    //         .body()
-    //         .expect("document body")
-    //         .into();
-    //     self.mount_to_element(root);
-    // }
-
     fn mount_to_element(root: web_sys::Element) {
         let rc_comp = crate::component::RcComp::new(Some(root));
-        let state = Self::with_comp(rc_comp.comp()).expect_throw("Component::with_comp() should be implemented (return Some(Self)) for the main component");
+        let state = Self::with_comp(rc_comp.comp())
+            .expect_throw("Component::with_comp() should be implemented to return `Some(Self)` for the main component");
         rc_comp.set_state(state);
         rc_comp.first_render();
 
