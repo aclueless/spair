fcf05a94695e6e60443ff8e28dc9456f06fb8cb6
aclueless <61309385+aclueless@users.noreply.github.com>
2020-09-03 14:44:53 +0700

    disable test on firefox

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ cargo clippy -- -D warnings
 
 # --chrome and --firefox on separate lines to easily disable one of them if the drive has problems
 wasm-pack test --headless --chrome -- --features="keyed-list"
-wasm-pack test --headless --firefox -- --features="keyed-list"
+#wasm-pack test --headless --firefox -- --features="keyed-list"
 
 for x in ./examples/*; do
     if [ -f $x/Cargo.toml ]; then
