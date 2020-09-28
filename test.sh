fb691509945ab5cc88d714120413602d636130d2
aclueless <61309385+aclueless@users.noreply.github.com>
2020-09-28 22:48:49 +0700

    re enable tests on firefox

--- test.sh
+++ test.sh
@@ -11,9 +11,9 @@ cargo clippy -- -D warnings
 wasm-pack test --headless --chrome -- --features="keyed-list"
 wasm-pack test --headless --chrome -- --features="fetch-json" # just build, no actual tests yet
 wasm-pack test --headless --chrome -- --features="fetch-ron" # just build, no actual tests yet
-# wasm-pack test --headless --firefox -- --features="keyed-list"
-# wasm-pack test --headless --firefox -- --features="fetch-json" # just build, no actual tests yet
-# wasm-pack test --headless --firefox -- --features="fetch-ron" # just build, no actual tests yet
+wasm-pack test --headless --firefox -- --features="keyed-list"
+wasm-pack test --headless --firefox -- --features="fetch-json" # just build, no actual tests yet
+wasm-pack test --headless --firefox -- --features="fetch-ron" # just build, no actual tests yet
 
 for x in ./examples/*; do
     if [ -f $x/Cargo.toml ]; then
