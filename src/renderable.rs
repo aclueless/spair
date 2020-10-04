037b7c568fa46f404d0d0a4afe546e3934f505e1
aclueless <61309385+aclueless@users.noreply.github.com>
2020-10-04 15:35:18 +0700

    add back impl ListItem for &T

--- src/renderable.rs
+++ src/renderable.rs
@@ -135,3 +135,10 @@ pub trait ListItem<C: crate::component::Component> {
     const ROOT_ELEMENT_TAG: &'static str;
     fn render(&self, item: crate::dom::ElementUpdater<C>);
 }
+
+impl<C: crate::component::Component, T: ListItem<C>> ListItem<C> for &T {
+    const ROOT_ELEMENT_TAG: &'static str = T::ROOT_ELEMENT_TAG;
+    fn render(&self, item: crate::dom::ElementUpdater<C>) {
+        (*self).render(item);
+    }
+}
