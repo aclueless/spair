85b04937eb4db0f7c5f2881083fefe73805d4d0d
aclueless <61309385+aclueless@users.noreply.github.com>
2020-10-09 11:55:53 +0700

    try compile partial non keyed list

--- test.sh
+++ test.sh
@@ -8,10 +8,12 @@ cargo clippy -- -D warnings
 
 # --chrome and --firefox on separate lines to easily disable one of them if the drive has problems
 # wasm-pack test --headless --chrome -- --all-features
-wasm-pack test --headless --chrome -- --features="keyed-list"
-wasm-pack test --headless --chrome -- --features="fetch-json" # just build, no actual tests yet
-wasm-pack test --headless --chrome -- --features="fetch-ron" # just build, no actual tests yet
+# wasm-pack test --headless --chrome -- --features="keyed-list"
+# wasm-pack test --headless --chrome -- --features="partial-non-keyed-list" # just build, no actual tests yet
+# wasm-pack test --headless --chrome -- --features="fetch-json" # just build, no actual tests yet
+# wasm-pack test --headless --chrome -- --features="fetch-ron" # just build, no actual tests yet
 wasm-pack test --headless --firefox -- --features="keyed-list"
+wasm-pack test --headless --firefox -- --features="partial-non-keyed-list" # just build, no actual tests yet
 wasm-pack test --headless --firefox -- --features="fetch-json" # just build, no actual tests yet
 wasm-pack test --headless --firefox -- --features="fetch-ron" # just build, no actual tests yet
 
