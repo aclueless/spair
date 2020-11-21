f177c544414f9e9a435e6a6ece2929a0f85bae0b
aclueless <61309385+aclueless@users.noreply.github.com>
2020-11-21 12:12:44 +0700

    wip svg elements

--- src/dom/svg/nodes.rs
+++ src/dom/svg/nodes.rs
@@ -130,6 +130,7 @@ pub trait SvgBuilder<C: crate::component::Component>: Sized {
         this
     }
 
+    // https://developer.mozilla.org/en-US/docs/Web/SVG/Element
     create_methods_for_tags! {
         a
         animate
@@ -139,22 +140,69 @@ pub trait SvgBuilder<C: crate::component::Component>: Sized {
         clip_path "clipPath"
         defs
         desc
-        filter
+        discard
+        ellipse
+        fe_blend "feBlend"
+        fe_color_matrix "feColorMatrix"
+        fe_component_transfer "feComponentTransfer"
+        fe_composite "feComposite"
+        fe_convolve_matrix "feConvolveMatrix"
+        fe_diffuse_lighting "feDiffuseLighting"
+        fe_displacement_map "feDisplacementMap"
+        fe_distant_light "feDistantLight"
+        fe_drop_shadow "feDropShadow"
+        fe_flood "feFlood"
+        fe_func_a "feFuncA"
+        fe_func_b "feFuncB"
+        fe_func_g "feFuncG"
+        fe_func_r "feFuncR"
         fe_gaussian_blur "feGaussianBlur"
+        fe_image "feImage"
+        fe_merge "feMerge"
+        fe_merge_node "feMergeNode"
+        fe_morphology "feMorphology"
         fe_offset "feOffset"
+        fe_point_light "fePointLight"
+        fe_specular_lighting "feSpecularLighting"
+        fe_spot_light "feSpotLight"
+        fe_tile "feTile"
+        fe_turbulence "feTurbulence"
+        filter
+        foreign_object "foreignObject"
         g
+        hatch
+        hatchpath
+        image
         line
-    }
-
-    fn svg(self, f: impl FnOnce(super::SvgUpdater<C>)) -> Self::Output {
-        use sealed::SvgBuilder;
-        let mut this: Self::Output = self.into();
-        if this.require_render() {
-            f(this.get_element_and_increase_index("svg"));
-        } else {
-            this.next_index();
-        }
-        this
+        linear_gradient "linearGradient"
+        marker
+        mask
+        mesh
+        meshgradient
+        meshpatch
+        meshrow
+        metadata
+        mpath
+        path
+        pattern
+        polygon
+        polyline
+        radial_gradient "radialGradient"
+        rect
+        //script ??
+        set
+        solidcolor
+        stop
+        style_element "style" // conflict with attribute with the same name
+        svg
+        switch
+        symbol
+        text
+        text_path "textPath"
+        title
+        tspan
+        r#use "use"
+        view
     }
 }
 
